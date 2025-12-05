b66f281d8de8d8a1b65ecc9c499a7b84ab1d9544
akshay-moolath <akmoolath@gmail.com>
2025-12-05 21:41:36 +0530

    removed vercel.json and check threads

--- app/db.py
+++ app/db.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL,connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 #create session for requesting data from database
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
